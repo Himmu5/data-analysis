c84217f1c5c1ca8a3713e38ff1a00a4a12e48e34
Himanshu Chauhan <adityachauhan5566@gmail.com>
2025-09-11 08:08:02 +0530

    feat: scrapper run

--- project/main.py
+++ project/main.py
@@ -1,2 +1,5 @@
+from scrapper import Scrapper
 
 
+scrapper = Scrapper()
+scrapper.scrap_cricket_data()
--- project/scrapper.py
+++ project/scrapper.py
@@ -4,12 +4,11 @@ import requests
 class Scrapper:
     url = 'https://www.espncricinfo.com/records/tournament/team-match-results/icc-men-s-t20-world-cup-2022-23-14450'
 
-    def __init__(self, name, age):
+    def __init__(self):
       pass
     
     def scrap_cricket_data(self):
-        response = requests(self.url)
+        response = requests.get(self.url)
         html_content = response.text
         soup = BeautifulSoup(html_content, 'html.parser')
         print(soup)
-        
